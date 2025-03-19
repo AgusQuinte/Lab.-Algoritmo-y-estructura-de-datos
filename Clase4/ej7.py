d731a30c018305fc760a4febf9c412920873870e
Agus Quintela <agusquintela08@gmail.com>
2025-03-19 10:12:40 -0300

    Agregar valores a lista

---
+++ Clase4/ej7.py
@@ -0,0 +1,6 @@
+usuarios = []
+print("Bienvenido, ingrese su nombre y el de 2 amigos mas")
+for i in range (3):
+    nombre = input("ingrese nombres:" )
+    usuarios.append(nombre)
+print(usuarios)
