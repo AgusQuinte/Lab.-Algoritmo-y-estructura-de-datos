69baea11563d52c299e21ec25eab67e733b2f437
Agus Quintela <agusquintela08@gmail.com>
2025-03-19 11:13:38 -0300

    Mensajes

--- Clase4/ej7.py
+++ Clase4/ej7.py
@@ -3,4 +3,5 @@ print("Bienvenido, ingrese su nombre y el de 2 amigos mas")
 for i in range (3):
     nombre = input("ingrese nombres:" )
     usuarios.append(nombre)
+    
 print(usuarios)
---
+++ Clase4/ej8.py
@@ -0,0 +1,6 @@
+mensaje = ["Buenas noches, que lindo que estes acompanandome esta noche aca, estoy muy agradecido.", "Holaaa, gracias por venir, te extrañe un montoon", "Como estas? Todo bien? Hacia mucho tiempo no te veia por aca."]
+personas = ["Juan", "Micaela", "jorge"]
+
+print(f"Mensaje para {personas[0]}: {mensaje[0]}")
+print(f"Mensaje para {personas[1]}: {mensaje[1]}")
+print(f"Mensaje para {personas[2]}: {mensaje[2]}")
