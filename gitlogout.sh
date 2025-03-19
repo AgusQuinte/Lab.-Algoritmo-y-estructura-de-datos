3aba6757ac7f0ff86d9691695cd639f5a79390dd
AgusQuinte <agusquintela08@gmail.com>
2025-03-19 09:19:02 -0300

    Delete gitlogout.sh

--- gitlogout.sh
+++
@@ -1,8 +0,0 @@
-git config --global --unset user.name
-git config --global --unset user.email
-git config --global --unset credential.helper
-cmdkey /delete:LegacyGeneric:target=git:https://github.com
-
-
-git config --global user.name " AgusQuinte"
-git config --global user.email " agusquintela08@gmail.com "
