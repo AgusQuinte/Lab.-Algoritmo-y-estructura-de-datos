61aa0b2dc14bc0c97a1008c878ac22823a7a4958
AgusQuinte <agusquintela08@gmail.com>
2025-03-21 18:18:29 -0300

    hola

---
+++ Clase5/ej13.1.py
@@ -0,0 +1,10 @@
+color_alien = "amarillo"
+color_alien_2 = "verde"
+
+if color_alien == "verde":
+    print("ganaste 5 puntos")
+    
+print("siguiente programa")
+
+if color_alien_2 == "verde":
+    print("ganaste 5 puntos")
