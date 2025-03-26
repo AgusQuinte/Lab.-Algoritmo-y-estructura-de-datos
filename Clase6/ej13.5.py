635df7a20253730d36de9ec4eadc56ca0f5f8581
AgusQuinte <agusquintela08@gmail.com>
2025-03-26 09:51:48 -0300

    fruta

---
+++ Clase6/ej13.5.py
@@ -0,0 +1,13 @@
+frutas_fav = ["melon", "banana", "frutilla"]
+
+for fruta in frutas_fav:
+    if fruta == "banana":
+        print("Me encanta la banana")
+    elif fruta == "mandarina":
+        print("Me encanta la mandarina")
+    elif fruta == "sandia":
+        print("Me encanta la sandia")
+    elif fruta == "melon":
+        print("Me encanta el melon")
+    elif fruta == "frutilla":
+        print("Me encanta la frutilla")
