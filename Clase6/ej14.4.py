1af2f02da75dfef2dacd89913ab4dd73606e0c4d
AgusQuinte <agusquintela08@gmail.com>
2025-03-26 11:41:01 -0300

    ordinales

---
+++ Clase6/ej14.4.py
@@ -0,0 +1,15 @@
+numeros = [1,2,3,4,5,6,7,8,9]
+
+for num in numeros:
+    if num == 1 or num ==3:
+        print(num,"ero")
+    elif num == 2:
+        print(num,"do")
+    elif num == 4 or num == 5 or num == 6:
+        print(num, "to")
+    elif num == 7:
+        print(num, "mo")
+    elif num == 8:
+        print(num, "vo")
+    else:
+        print(num, "no")
